8a300598d72d4e62ad20c5c9e2a04c35c599974f
iftekharr001 <iftekharr001@gmail.com>
2023-08-20 00:53:22 -0700

    added parallelogram card

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -27,3 +27,35 @@ function calculateRectangleArea(){
     const rectangleArea = document.getElementById('ractangle-area');
     rectangleArea.innerText = area;
 }
+
+// reusable fanction
+
+function calculateParallelogramArea(){
+    const base = getInputValue('parallelogram-base');
+
+    const height = getInputValue('parallelogram-height')
+
+    const area = base * height;
+    setElementInnerText('parallelogram-area', area)
+}
+
+
+
+function calculateEllipseArea(){
+    const majorRedius = getInputValue('ellipse-major-redius')
+    const mainorRedius = getInputValue('ellipse-minor-redius')
+    const area = 3.14 * majorRedius * mainorRedius;
+    setElementInnerText('ellipse-area', area)
+}
+
+function getInputValue(fieldId) {
+    const inputFiled = document.getElementById(fieldId);
+    const inputValueText = inputFiled.value;
+    const value = parseFloat(inputValueText)
+    return value;
+}
+// reusable set 
+function setElementInnerText(elementId, area) {
+    const element = document.getElementById(elementId, area);
+    element.innerText = area;
+}
